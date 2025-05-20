9372de460c049bb4b4f299f88dc1ac9f767d5719
gaspar-ds <gaspardoval1@gmail.com>
2025-05-20 10:21:36 -0300

    Usar process.env.PORT para Render

--- app.js
+++ app.js
@@ -51,5 +51,5 @@ app.post('/contacto', async (req, res) => {
 // Servidor
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-    console.log(`Servidor funcionando en http://localhost:${PORT}`);
+  console.log(`Servidor escuchando en el puerto ${PORT}`);
 });
